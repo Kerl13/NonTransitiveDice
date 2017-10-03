fefdec0269b4430539ebef6c74fbc0ca86ecdeea
Martin Pépin <martin.pepin@ens.fr>
2017-10-03 15:48:25 +0200

    optimize check.pl a little

--- src/check.pl
+++ src/check.pl
@@ -7,13 +7,13 @@
 %% Entrypoint
 
 % Remember the first dice
-check_dice([A | L]) :- check_dice_aux(A, [A | L]).
+check_dice([A | L]) :- check_dice_aux([A | L], A).
 
 % First argument is the first dice of the cycle
-check_dice_aux(Fst, [Last]) :- beats(Last, Fst).
-check_dice_aux(Fst, [A1 | [A2 | L]]) :-
+check_dice_aux([Last], Fst) :- beats(Last, Fst).
+check_dice_aux([A1 | [A2 | L]], Fst) :-
    beats(A1, A2),
-   check_dice_aux(Fst, [A2 | L]).
+   check_dice_aux([A2 | L], Fst).
 
 
 
@@ -30,8 +30,13 @@ a_wins_b_wins([], 0, 0).
 a_wins_b_wins([[X, Y] | Comb], Awins, Bwins) :-
    X > Y,
    a_wins_b_wins(Comb, Awins_minus_1, Bwins),
-   Awins is 1 + Awins_minus_1.
+   Awins is 1 + Awins_minus_1,
+   !.
 a_wins_b_wins([[X, Y] | Comb], Awins, Bwins) :-
    X < Y,
    a_wins_b_wins(Comb, Awins, Bwins_minus_1),
-   Bwins is 1 + Bwins_minus_1.
+   Bwins is 1 + Bwins_minus_1,
+   !.
+a_wins_b_wins([[X, X] | Comb], Awins, Bwins) :-
+   a_wins_b_wins(Comb, Awins, Bwins),
+   !.
